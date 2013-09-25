d854570fc44e096044b62caa73106ab52be7421e
Ismael <ijimenezgarzon@gmail.com>
2013-09-24 18:54:17 -0600

    show result at page

--- web.js
+++ web.js
@@ -5,13 +5,13 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Probando!!!');
+ // response.send('Probando!!!');
+	response.send(connectDB());
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-  connectDB();
 });
 
 
@@ -21,7 +21,8 @@ function connectDB()
    client.query('SELECT * FROM requests', function(err, result) {
     done();
     if(err) return console.error(err);
-    console.log(result.rows);
+    //console.log(result.rows);
+	return result.rows;
    });
   });
 }
