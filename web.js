23a377e18e0ae27a862d4a09eb6c2d7c86516109
Ismael <ijimenezgarzon@gmail.com>
2013-09-24 18:50:15 -0600

    pg added to the installation

--- web.js
+++ web.js
@@ -1,4 +1,6 @@
 var express = require("express");
+var pg = require("pg");
+
 var app = express();
 app.use(express.logger());
 
@@ -9,4 +11,17 @@ app.get('/', function(request, response) {
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
+  connectDB();
 });
+
+
+function connectDB()
+{
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+   client.query('SELECT * FROM requests', function(err, result) {
+    done();
+    if(err) return console.error(err);
+    console.log(result.rows);
+   });
+  });
+}
