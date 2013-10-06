115b2b925246e28b116603eecf8210556a191714
Ismael <ijimenezgarzon@gmail.com>
2013-10-06 15:03:11 -0600

    new change 4

---
+++ data.js
@@ -0,0 +1,15 @@
+//Module for data access functions
+
+
+function connectDB(response)
+{
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+   client.query('SELECT * FROM requests', function(err, result) {
+    
+    if(err) return console.error(err);
+        console.log(result.rows);
+	
+	response.send(result.rows);
+   });
+  });
+}
