0a99dc623923c7791dfc5e376e41f21731e5f890
Ismael <ijimenezgarzon@gmail.com>
2013-10-06 15:34:23 -0600

    new change 7

--- data.js
+++ data.js
@@ -1,6 +1,9 @@
 //Module for data access functions
 var method = Data.prototype;
 
+var strIns = 'insert into products(json) values(\'{"type":"television", "price": 899.99, "resolution":"1080p"}\'),   (\'{"type":"shoe", "price": 74.99, "size":10.5}\')';
+
+
 function Data()
 {
 	
@@ -18,5 +21,26 @@ Data.connectDB = function(pg,response)
    });
   });
 };
+
+
+Data.insertJSON = function(pg,response)
+{
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+   client.query( strIns , function(err, result) {
+    
+    if(err) return console.error(err);
+        console.log(result.rows);
+	
+	response.send(result.rows);
+   });
+  });
+};
+
+
+
+
+
+
+
 
 module.exports = Data;
