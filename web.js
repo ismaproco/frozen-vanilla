11333972c7da7b331c2199dadf51a879f8f6fb14
OpenShift guest <ismaproco@gmail.com>
2014-01-06 09:54:22 -0500

    Fix template project

--- web.js
+++ web.js
@@ -1,40 +1,12 @@
-var express = require("express");
-var pg = require("pg");
-
-//Internal vars
-var data = require("./data.js");
-//
-
-
+var express = require('express');
 var app = express();
 app.use(express.logger());
 
-app.get('/allItems', function(request, response) {
- // response.send('Probando!!!');
-	var item = data.connectDB(pg,response);
-	console.log(item);
-	
-});
-
-app.get('/insert', function(request, response) {
-	data.insertJSON(pg,response,request);
-	response.send('Insert OK');
-});
-
-app.post('/insert', function(request, response) {
-	data.insertJSON(pg,response,request);
-	response.send('Insert OK');
-});
-
 app.get('/', function(request, response) {
-	response.send('Don\'t trust in this message!!!');
+  response.send('Hello World!');
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
-
