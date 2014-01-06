50dcd9df435a3709c16f7e826920a8cda0b6b207
OpenShift guest <ismaproco@gmail.com>
2014-01-06 10:16:39 -0500

    Changes  on the web server and file management

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var app = express();
-app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
+
+app.use(express.logger());
+app.use('/static', express.static(__dirname + '/public'));
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
