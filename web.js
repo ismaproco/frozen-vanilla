5495b33024937bb303f5a83f672a86565a1d402d
OpenShift guest <ismaproco@gmail.com>
2014-01-06 10:27:44 -0500

    Fixing the server

--- web.js
+++ web.js
@@ -3,10 +3,11 @@ var app = express();
 
 
 app.use(express.logger());
-app.use('/static', express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 
 var port = process.env.PORT || 5000;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
