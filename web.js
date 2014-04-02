38eefbd92d184936836d32970889021c880c55ae
ismaproco <ijimenezgarzon@gmail.com>
2014-04-02 09:08:39 -0700

    Added passport credentials

--- web.js
+++ web.js
@@ -19,11 +19,53 @@ app.use(express.static(__dirname + '/public', {
 //
 
 
+/** INIT PASSPORT CODE**/
+
+var passport = require('passport')
+  , GoogleStrategy = require('passport-google').Strategy;
+
+passport.use(new GoogleStrategy({
+    returnURL: 'http://frozenvanilla.herokuapp.com/auth/google/return',
+    realm: 'http://frozenvanilla.herokuapp.com/tindex.html'
+  },
+  function(identifier, profile, done) {
+    User.findOrCreate({ openId: identifier }, function(err, user) {
+      done(err, user);
+    });
+  }
+));
+
+/** END  PASSPORT CODE**/
+
 var cm = new mongo();
 cm.init();
 
 app.use(express.logger());
 app.use(express.static(__dirname + '/public'));
+
+
+//ROUTES START
+
+
+//**** PASSPORT ROUTES****/
+
+// Redirect the user to Google for authentication.  When complete, Google
+// will redirect the user back to the application at
+//     /auth/google/return
+app.get('/auth/google', passport.authenticate('google'));
+
+// Google will redirect the user to this URL after authentication.  Finish
+// the process by verifying the assertion.  If valid, the user will be
+// logged in.  Otherwise, authentication has failed.
+app.get('/auth/google/return', 
+  passport.authenticate('google', { successRedirect: '/',
+                                    failureRedirect: '/login' }));
+
+
+//***********************//
+
+
+
 
 
 
