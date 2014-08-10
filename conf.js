531cd9b18aebc71cc1ee5148c07465a58e0c3892
Ismael Jimenez <ismaproco@users.noreply.github.com>
2014-08-10 10:50:06 -0600

    Update conf.js
    remove old urls

--- conf.js
+++ conf.js
@@ -1,9 +1,6 @@
 module.exports = {
-	MONGO_URL: "mongodb://ronock:pasteleria-de-la-45@flame.mongohq.com:27053/primera",
-	MAIN_COLLECTION_NAME: "processedInternetItemCollection"
+	MONGO_URL: "mongodb://localhost/mydb",
+	MAIN_COLLECTION_NAME: "persons"
 }
 /*	LOCAL DB		*/
-/*        MONGO_URL:"mongodb://localhost/primera",      */
-
-/*	EXTERNAL DB		*/
-/*MONGO_URL:"mongodb://ronock:pasteleria-de-la-45@flame.mongohq.com:27053/primera",*/
+/*        MONGO_URL:"mongodb://localhost/mydb",      */
